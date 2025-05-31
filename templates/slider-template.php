1146653c51b29f18d88b653e1bd8910d1b3f65a7
akashbhowmik54 <bhowmikakash67@gmail.com>
2025-05-31 20:50:42 +0600

    Change classname naming pattern

--- templates/slider-template.php
+++ templates/slider-template.php
@@ -4,16 +4,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<div class="cslpr__slider-wrapper">
-    <div class="cslpr__slider">
+<div class="cslpr-slider-wrapper">
+    <div class="cslpr-slider">
         <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-            <div class="cslpr__slide">
+            <div class="cslpr-slide">
                 <?php if ( has_post_thumbnail() ) : ?>
-                    <div class="cslpr__slide-image">
+                    <div class="cslpr-slide-image">
                         <?php the_post_thumbnail( 'large' ); ?>
                     </div>
                 <?php endif; ?>
-                <h3 class="cslpr__slide-title"><?php the_title(); ?></h3>
+                <h3 class="cslpr-slide-title"><?php the_title(); ?></h3>
             </div>
         <?php endwhile; ?>
     </div>
